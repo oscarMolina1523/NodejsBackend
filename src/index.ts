1bf30b753401c7337191741e71a144e60dc2fed2
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-14 19:31:54 -0600

    adding a new code to read the body of all responses in json format

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
