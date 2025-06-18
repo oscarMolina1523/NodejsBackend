fb88c286753004003f56d2fa5ae045d2acc228bd
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-18 13:44:21 -0600

    type of students fixed

--- src/data/studentsData.ts
+++ src/data/studentsData.ts
@@ -1,6 +1,6 @@
 import { Student } from "../models/Student";
 
-export let students:[Student];
+export let students: Student[];
 
 students =[
     {fullName: "Alice Rodger", age: 20, grade: "First Grade", gender:"Female"},
