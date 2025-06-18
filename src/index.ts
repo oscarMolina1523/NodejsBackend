19c7b6d15eb2d2225992d9f15ed9d49217376e6f
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-18 17:24:55 -0600

    adding the code to use the swagger

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import studentsRoutes from './routes/StudentsRoutes';
+import swaggerUI from 'swagger-ui-express';
+import specs from './swagger/swagger';
 
 const app = express();
 const PORT = 3000;
+
 app.use(express.json());
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 app.use('/', studentsRoutes);
 
 app.listen(PORT, () => {
