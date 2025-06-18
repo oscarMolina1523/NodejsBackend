027020c9da5b6fa7031cb75ca0473c1223d12d6c
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-18 13:58:06 -0600

    implementing routes of students in principal file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import studentsRoutes from './routes/StudentsRoutes';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+app.use('/', studentsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
