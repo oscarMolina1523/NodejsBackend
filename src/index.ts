affa92d3e4ac8cab117dce7825a23839def62e67
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-18 17:20:55 -0600

    unique port to the app is 3000

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import studentsRoutes from './routes/StudentsRoutes';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.use(express.json());
 app.use('/', studentsRoutes);
 
