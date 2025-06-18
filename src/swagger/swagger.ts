c0612c24285949c859ee3da26552bec7332990fd
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-18 17:21:33 -0600

    adding the configurations to read routes defined

---
+++ src/swagger/swagger.ts
@@ -0,0 +1,25 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Asistent API',
+            version: '1.0.0',
+            description: 'API for managing Asistent',
+            contact: {
+                name: 'Oscar Molina'
+            },
+            servers: [
+                {
+                    url: 'http://localhost:3000',
+                    description: 'Local server'
+                }
+            ]
+        }
+    },
+    apis: ['./routes/*.ts'], 
+};
+
+const specs = swaggerJsdoc(options);
+export default specs;
