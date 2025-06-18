b1e9a08da16acf4daa1e1dd01448ced5f94c095b
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-18 13:44:36 -0600

    adding type for response

--- src/services/StudentsService.ts
+++ src/services/StudentsService.ts
@@ -1,3 +1,4 @@
 import { students } from "../data/studentsData";
+import { Student } from "../models/Student";
 //we send the array of students to the client
-export const getStudents = () => students;
+export const getStudents = (): Student[] => students;
