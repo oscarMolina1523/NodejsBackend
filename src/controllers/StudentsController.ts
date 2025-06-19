774d0021b7849ea088d9ac2d47662a87d655dd2c
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-19 11:23:29 -0600

    creating a function in student controller to delete a student from the array

--- src/controllers/StudentsController.ts
+++ src/controllers/StudentsController.ts
@@ -3,7 +3,8 @@ import {
   getStudents,
   getStudentById,
   addStudent,
-  updateStudent
+  updateStudent,
+  deleteStudent,
 } from "../services/StudentsService";
 import { Request, Response } from "express";
 import { generateId } from "../utils/GenerateId";
@@ -31,25 +32,34 @@ export const addAStudent = (req: Request, res: Response) => {
     age: newStudent.age,
     gender: newStudent.gender,
     grade: newStudent.grade,
-  }
+  };
   const result = addStudent(completeStudent);
   if (result) {
     res
       .status(201)
       .json({ message: "Student added successfully", student: newStudent });
-  }
-  else{
+  } else {
     res.status(400).json({ message: "Failed to add student" });
   }
 };
 
 export const updateAStudent = (req: Request, res: Response) => {
-    const studentId = req.params.id;
-    const updatedStudent: StudentDTO = req.body;
-    const response = updateStudent(studentId, updatedStudent);
-    if (response.success) {
-      res.status(200).json({ message: response.message });
-    } else {
-      res.status(404).json({ message: response.message });
-    }
+  const studentId = req.params.id;
+  const updatedStudent: StudentDTO = req.body;
+  const response = updateStudent(studentId, updatedStudent);
+  if (response.success) {
+    res.status(200).json({ message: response.message });
+  } else {
+    res.status(404).json({ message: response.message });
+  }
+};
+
+export const deleteAStudent = (req: Request, res: Response) => {
+  const studentId: string = req.params.id;
+  const response = deleteStudent(studentId);
+  if (response.success) {
+    res.status(200).json({ message: response.message });
+  } else {
+    res.status(404).json({ message: response.message });
+  }
 };
