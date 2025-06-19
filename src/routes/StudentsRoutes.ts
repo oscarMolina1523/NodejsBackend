be8a7a9c921e80d46bd43086cf587dff08ae5207
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-18 18:44:58 -0600

    adding the correct route to get students in endpoint

--- src/routes/StudentsRoutes.ts
+++ src/routes/StudentsRoutes.ts
@@ -1,8 +1,67 @@
-import  express  from "express";
+import express from "express";
 import { getAllStudents } from "../controllers/StudentsController";
 
-const router= express.Router();
+const router = express.Router();
 
-router.get("/", getAllStudents);
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Student:
+ *       type: object
+ *       required:
+ *         - fullName
+ *         - age
+ *         - grade
+ *         - gender
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the student
+ *         fullName:
+ *           type: string
+ *           description: The full name of the student
+ *         age:
+ *           type: integer
+ *           description: The age of the student
+ *         grade:
+ *           type: string
+ *           description: The id grade of the student
+ *         gender:
+ *           type: string
+ *           description: The gender of the student
+ *       example:
+ *         id: kskbVGJV4G6T
+ *         fullName: Ted Junior Rodgers
+ *         age: 10
+ *         grade: khabckGCJVgc6
+ *         gender: male
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Students
+ *   description: The Students managing API
+ */
+
+/**
+ * @swagger
+ * /students:
+ *   get:
+ *     summary: Returns the list of all the Students
+ *     tags: [Students]
+ *     responses:
+ *       200:
+ *         description: The list of the Students
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Student'
+ */
+
+router.get("/students", getAllStudents);
 
 export default router;
