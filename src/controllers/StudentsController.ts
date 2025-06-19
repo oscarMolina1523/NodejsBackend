ba353ce28c1b0989d2c5ed8f2bcc670a96eabb1f
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-18 20:30:00 -0600

    adding a function to get a student by id

--- src/controllers/StudentsController.ts
+++ src/controllers/StudentsController.ts
@@ -1,8 +1,19 @@
 import { Student } from "../models/Student";
-import { getStudents } from "../services/StudentsService";
+import { getStudents, getStudentById } from "../services/StudentsService";
 import { Request, Response } from "express";
 
 export const getAllStudents=(req:Request, res:Response)=>{
     const students: Student[] = getStudents();
     res.status(200).json(students);
+}
+
+export const getAStudentById=(req:Request, res:Response)=>{
+    const studentId = req.params.id;
+    const student= getStudentById(studentId);
+    if(student){
+        res.status(200).json(student);
+    }
+    else{
+        res.status(404).json({ message: "Student not found" });
+    }
 }
