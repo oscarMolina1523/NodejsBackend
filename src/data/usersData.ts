398dae2168ac2e9b553d5c9f4d581b2530d91ba0
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-19 15:42:44 -0600

    creating a file with static data of users

---
+++ src/data/usersData.ts
@@ -0,0 +1,8 @@
+import { User } from "../models/User";
+
+export let users: User[];
+
+users = [
+  { id: "user1", fullName: "Alice Smith", password: "30", email: "alice@example", role: "admin" },
+  { id: "user2", fullName: "Bob Johnson", password:"25", email: "", role: "tutor" },
+];
