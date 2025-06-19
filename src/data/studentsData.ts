e2824fbbc14f80b44a458a3c661c1ce698d39b00
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-18 18:44:13 -0600

    adding an id to the data of users

--- src/data/studentsData.ts
+++ src/data/studentsData.ts
@@ -3,5 +3,5 @@ import { Student } from "../models/Student";
 export let students: Student[];
 
 students =[
-    {fullName: "Alice Rodger", age: 20, grade: "First Grade", gender:"Female"},
+    {id:"ghavgjVJVHVG76",fullName: "Alice Rodger", age: 20, grade: "First Grade", gender:"Female"},
 ]
