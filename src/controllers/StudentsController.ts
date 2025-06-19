dc24b4625070bbfe74aaec82fabc5cf8c00849a6
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-18 20:58:25 -0600

    adding a new fuction in controller to add a student

--- src/controllers/StudentsController.ts
+++ src/controllers/StudentsController.ts
@@ -1,19 +1,35 @@
 import { Student } from "../models/Student";
-import { getStudents, getStudentById } from "../services/StudentsService";
+import {
+  getStudents,
+  getStudentById,
+  addStudent,
+} from "../services/StudentsService";
 import { Request, Response } from "express";
 
-export const getAllStudents=(req:Request, res:Response)=>{
-    const students: Student[] = getStudents();
-    res.status(200).json(students);
-}
+export const getAllStudents = (req: Request, res: Response) => {
+  const students: Student[] = getStudents();
+  res.status(200).json(students);
+};
 
-export const getAStudentById=(req:Request, res:Response)=>{
-    const studentId = req.params.id;
-    const student= getStudentById(studentId);
-    if(student){
-        res.status(200).json(student);
-    }
-    else{
-        res.status(404).json({ message: "Student not found" });
-    }
-}
+export const getAStudentById = (req: Request, res: Response) => {
+  const studentId = req.params.id;
+  const student = getStudentById(studentId);
+  if (student) {
+    res.status(200).json(student);
+  } else {
+    res.status(404).json({ message: "Student not found" });
+  }
+};
+
+export const addAStudent = (req: Request, res: Response) => {
+  const newStudent: Student = req.body;
+  const result = addStudent(newStudent);
+  if (result) {
+    res
+      .status(201)
+      .json({ message: "Student added successfully", student: newStudent });
+  }
+  else{
+    res.status(400).json({ message: "Failed to add student" });
+  }
+};
