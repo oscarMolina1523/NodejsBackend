6b02017b9a017d6e79d0d18ca02d6452f9972238
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-18 19:49:13 -0600

    adding new function to get student by id in student services

--- src/services/StudentsService.ts
+++ src/services/StudentsService.ts
@@ -2,3 +2,7 @@ import { students } from "../data/studentsData";
 import { Student } from "../models/Student";
 //we send the array of students to the client
 export const getStudents = (): Student[] => students;
+
+export const getStudentById = (id: string): Student | undefined => {
+  return students.find((student) => student.id === id);
+};
