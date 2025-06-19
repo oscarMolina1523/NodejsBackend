79b822ef2a3042e03deaa86e2dd7b359e862e864
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-18 20:13:30 -0600

    adding validations to control exceptions

--- src/services/StudentsService.ts
+++ src/services/StudentsService.ts
@@ -5,32 +5,37 @@ export const getStudents = (): Student[] => students;
 
 //we send the specific student to the client by id
 export const getStudentById = (id: string): Student | undefined => {
-  return students.find((student) => student.id === id);
+  const student = students.find((student) => student.id === id);
+  if (!student) {
+    throw new Error(`Student with id ${id} not found`);
+  }
+  return student;
 };
 
 //we add a new student to the array of students
-export const addStudent = (student : Student): void =>{
-    students.push(student);
-}
+export const addStudent = (student: Student): void => {
+  students.push(student);
+};
 
 //we update an existing student in the array of students
-export const updateStudent= (id:string, updatedStudent: StudentDTO)=>{
-    const student = students.find((student) => student.id === id);
-    if(student){
-        student.fullName= updatedStudent.fullName;
-        student.age = updatedStudent.age;
-        student.gender = updatedStudent.gender;
-        student.grade = updatedStudent.grade;
-    }
-    else{
-        throw new Error(`Student with id ${id} not found`);
-    }
-}
+export const updateStudent = (id: string, updatedStudent: StudentDTO) => {
+  const student = students.find((student) => student.id === id);
+  if (student) {
+    student.fullName = updatedStudent.fullName;
+    student.age = updatedStudent.age;
+    student.gender = updatedStudent.gender;
+    student.grade = updatedStudent.grade;
+  } else {
+    throw new Error(`Student with id ${id} not found`);
+  }
+};
 
-//we delete a student using the id 
-export const deleteStudent =(id: string):void=>{
-    const student= students.findIndex((student) => student.id === id);
-    if(student !== -1){
-        students.splice(student, 1);
-    }
-}
+//we delete a student using the id
+export const deleteStudent = (id: string): void => {
+  const student = students.findIndex((student) => student.id === id);
+  if (student !== -1) {
+    students.splice(student, 1);
+  } else {
+    throw new Error(`Student with id ${id} not found`);
+  }
+};
