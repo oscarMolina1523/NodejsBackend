d202fed3816dcbc9cb4dc662d7941db7d246a327
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-19 16:09:54 -0600

    creating method to get all users

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,2 +1,15 @@
 import { Response, Request } from "express";
+import UserService from "../services/UserService";
 
+export default class UsersController {
+    private service : UserService;
+
+    constructor() {
+        this.service = new UserService();
+    }
+
+    getAllUsers=(req: Request, res: Response) => {
+        const users= this.service.getAllUsers();
+        res.status(200).json(users);
+    }
+}
