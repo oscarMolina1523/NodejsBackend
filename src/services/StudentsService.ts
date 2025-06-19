9b80891c4621c208bf5adfee079de48b162f4d43
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-18 20:03:02 -0600

    creating updated student function in services

--- src/services/StudentsService.ts
+++ src/services/StudentsService.ts
@@ -1,5 +1,5 @@
 import { students } from "../data/studentsData";
-import { Student } from "../models/Student";
+import { Student, StudentDTO } from "../models/Student";
 //we send the array of students to the client
 export const getStudents = (): Student[] => students;
 
@@ -9,4 +9,17 @@ export const getStudentById = (id: string): Student | undefined => {
 
 export const addStudent = (student : Student): void =>{
     students.push(student);
+}
+
+export const updateStudent= (id:string, updatedStudent: StudentDTO)=>{
+    const student = students.find((student) => student.id === id);
+    if(student){
+        student.fullName= updatedStudent.fullName;
+        student.age = updatedStudent.age;
+        student.gender = updatedStudent.gender;
+        student.grade = updatedStudent.grade;
+    }
+    else{
+        throw new Error(`Student with id ${id} not found`);
+    }
 }
