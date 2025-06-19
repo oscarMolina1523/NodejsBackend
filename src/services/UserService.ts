8fceae2a71c0470a4edb67ac7a469e670f79caef
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-19 16:05:56 -0600

    updating to class and instancing

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,5 +1,31 @@
 import { GenericCrudService } from "./GenericCrudService";
-import { User } from "../models/User";
+import { User, UserDTO } from "../models/User";
 import { usersData } from "../data/usersData";
 
-export const userService = new GenericCrudService<User>(usersData);
+export default class UserService {
+  private service: GenericCrudService<User>;
+
+  constructor() {
+    this.service = new GenericCrudService<User>(usersData);
+  }
+
+  getAllUsers() {
+    return this.service.getAll();
+  }
+
+  getUserById(id: string) {
+    return this.service.getById(id);
+  }
+
+  addUser(user: User) {
+    return this.service.add(user);
+  }
+
+  updateUser(id: string, user: UserDTO) {
+    return this.service.update(id, user);
+  }
+
+  deleteUser(id: string) {
+    return this.service.delete(id);
+  }
+}
