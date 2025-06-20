5e401ce4eb6bbda72221931c278711e7d890d444
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-06-19 18:07:44 -0600

    documenting get controller using swagger

--- src/routes/UsersRoutes.ts
+++ src/routes/UsersRoutes.ts
@@ -4,8 +4,96 @@ import UsersController from "../controllers/UsersController";
 const router = express.Router();
 const userController = new UsersController();
 
-router.get("/", userController.getAllUsers);
-router.get("/:id", userController.getUserById);
-router.post("/", userController.addUser);
-router.put("/:id", userController.updateUser);
-router.delete("/:id", userController.deleteUser);
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - fullName
+ *         - email
+ *         - password
+ *         - role
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the User
+ *         fullName:
+ *           type: string
+ *           description: The full name of the User
+ *         email:
+ *           type: string
+ *           description: The email of the User
+ *         password:
+ *           type: string
+ *           description: The password of the User
+ *         role:
+ *           type: string
+ *           description: The role of the User (e.g., admin, student, teacher)
+ *       example:
+ *         id: kskbVGJjgv
+ *         fullName: Eduardo Jose Rodriguez
+ *         email: eduardo@gmail.com
+ *         password: 123456
+ *         role: admin
+ *
+ *     UserDTO:
+ *       type: object
+ *       required:
+ *         - fullName
+ *         - email
+ *         - password
+ *         - role
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the User
+ *         fullName:
+ *           type: string
+ *           description: The full name of the User
+ *         email:
+ *           type: string
+ *           description: The email of the User
+ *         password:
+ *           type: string
+ *           description: The password of the User
+ *         role:
+ *           type: string
+ *           description: The role of the User (e.g., admin, student, teacher)
+ *       example:
+ *         id: kskbVGJjgv
+ *         fullName: Eduardo Jose Rodriguez
+ *         email: eduardo@gmail.com
+ *         password: 123456
+ *         role: admin
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: The Users managing API
+ */
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Returns the list of all the Users
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: The list of the Users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
+router.get("/users", userController.getAllUsers);
+router.get("/users/:id", userController.getUserById);
+router.post("/users", userController.addUser);
+router.put("/users/:id", userController.updateUser);
+router.delete("/users/:id", userController.deleteUser);
